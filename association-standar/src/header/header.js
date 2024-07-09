d39a34fcbdf1db387e3500439845938b14be6761
ObedOlavarria <129216824+ObedOlavarria@users.noreply.github.com>
2024-07-08 20:21:19 -0600

    Add files via upload

--- association-standar/src/header/header.js
+++ association-standar/src/header/header.js
@@ -1,6 +1,7 @@
 const toggleBtn = document.querySelector('.toggle_btn');
 const toggleBtnIcon = document.querySelector('.toggle_btn i');
 const dropDownMenu = document.querySelector('.dropdown_menu');
+const aboutMenus = document.querySelectorAll('.submenu');
 
 if (toggleBtn && toggleBtnIcon && dropDownMenu) {
   toggleBtn.onclick = function() {
@@ -9,3 +10,23 @@ if (toggleBtn && toggleBtnIcon && dropDownMenu) {
     toggleBtnIcon.className = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
   };
 }
+
+aboutMenus.forEach(menu => {
+  const actionBtn = menu.querySelector('.action_btn');
+  const dropdown = menu.querySelector('.dropdown');
+
+  if (actionBtn && dropdown) {
+    actionBtn.onclick = function(e) {
+      e.preventDefault();
+      dropdown.classList.toggle('open');
+
+      // Cerrar el menú desplegable al hacer clic en un enlace del menú "About"
+      const links = dropdown.querySelectorAll('a');
+      links.forEach(link => {
+        link.onclick = function() {
+          dropdown.classList.remove('open');
+        };
+      });
+    };
+  }
+});
